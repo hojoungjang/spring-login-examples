acd14e0cf3d05825f055c5da89dc2b9563ee71cf
Jang <hjjang501@gmail.com>
2025-01-21 16:11:02 +0900

    Add UserService

---
+++ src/main/java/com/github/hojoungjang/spring_login_examples/service/UserService.java
@@ -0,0 +1,21 @@
+package com.github.hojoungjang.spring_login_examples.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.github.hojoungjang.spring_login_examples.domain.User;
+import com.github.hojoungjang.spring_login_examples.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepo;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepo.findByEmail(email)
+            .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
