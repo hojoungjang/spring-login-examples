427c8e9485d4ba3887fbfb2fc2d660735d363b69
Jang <hjjang501@gmail.com>
2025-01-21 18:26:31 +0900

    Add logout functionality

--- src/main/java/com/github/hojoungjang/spring_login_examples/controller/UserApiController.java
+++ src/main/java/com/github/hojoungjang/spring_login_examples/controller/UserApiController.java
@@ -1,12 +1,19 @@
 package com.github.hojoungjang.spring_login_examples.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.github.hojoungjang.spring_login_examples.dto.AddUserRequest;
 import com.github.hojoungjang.spring_login_examples.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @RequiredArgsConstructor
@@ -20,4 +27,10 @@ public class UserApiController {
         return "redirect:/login";
     }
     
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, 
+            SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
 }
