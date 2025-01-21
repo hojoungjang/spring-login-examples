7a76a52fececdc60e3aa20e8cbb1b4f5b12536ee
Jang <hjjang501@gmail.com>
2025-01-21 15:45:14 +0900

    Initialize spring project

---
+++ src/main/java/com/github/hojoungjang/spring_login_examples/SpringLoginExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.hojoungjang.spring_login_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hojoungjang/spring_login_examples/SpringLoginExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hojoungjang.spring_login_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
